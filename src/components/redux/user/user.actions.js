6a256b773845a390dac82c64d853d0f20b07fb4d
Diyar Faraj <48604250+diyarfaraj@users.noreply.github.com>
2019-11-23 20:32:15 +0100

    added redirection if user is signed in, redirect them to hompage

--- src/components/redux/user/user.actions.js
+++ src/components/redux/user/user.actions.js
@@ -1,9 +1,11 @@
+import { UserActionTypes } from "./user.types";
+
 // functiion that returns objects
 // each object is in the correct format that the actions needs to be
 
 //here we hold the user thats created from the userAUth
 
 export const setCurrentUser = user => ({
-  type: "SET_CURRENT_USER",
+  type: UserActionTypes.SET_CURRENT_USER,
   payload: user
 });
