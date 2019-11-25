9550392d7c49f39da2484e5dc05a8a436b1098a1
Diyar Faraj <diyar.faraj@gmail.com>
2019-11-25 06:43:14 +0100

    created and styles cart dropdown

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,56 +1,59 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 //This is how we can modify our component to have access to redux
-import { connect } from "react-redux";
-import { ReactComponent as Logo } from "../../assets/crown.svg";
-import "./header.styles.scss";
-import { auth } from "./../../firebase/firebase.utils";
-import CartIcon from "../cart-icon/cart-icon.component";
+import { connect } from 'react-redux';
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+import './header.styles.scss';
+import { auth } from './../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+
+import CartDropdown from './../cart-dropdown/cart-dropdown.component';
 
 const Header = ({ currentUser }) => (
-  <div className="header">
-    <Link className="logo-container" to="/">
-      <Logo className="logo" />
-    </Link>
+	<div className="header">
+		<Link className="logo-container" to="/">
+			<Logo className="logo" />
+		</Link>
 
-    <div className="options">
-      {currentUser ? (
-        <div className="option">
-          {" "}
-          <span>Welcome {currentUser.displayName}</span>
-        </div>
-      ) : (
-        <div />
-      )}
-      <Link className="option" to="/shop">
-        {" "}
-        SHOP
-      </Link>
-      <Link className="option" to="/contact">
-        {" "}
-        CONTACT
-      </Link>
+		<div className="options">
+			{currentUser ? (
+				<div className="option">
+					{' '}
+					<span>Welcome {currentUser.displayName}</span>
+				</div>
+			) : (
+				<div />
+			)}
+			<Link className="option" to="/shop">
+				{' '}
+				SHOP
+			</Link>
+			<Link className="option" to="/contact">
+				{' '}
+				CONTACT
+			</Link>
 
-      {currentUser ? (
-        <div className="option" onClick={() => auth.signOut()}>
-          {" "}
-          SIGN OUT
-        </div>
-      ) : (
-        <Link className="option" to="/signin">
-          SIGN IN
-        </Link>
-      )}
-      <CartIcon />
-    </div>
-  </div>
+			{currentUser ? (
+				<div className="option" onClick={() => auth.signOut()}>
+					{' '}
+					SIGN OUT
+				</div>
+			) : (
+				<Link className="option" to="/signin">
+					SIGN IN
+				</Link>
+			)}
+			<CartIcon />
+		</div>
+		<CartDropdown />
+	</div>
 );
 
 //How to get the actual value of the user, as of right now is null
 // state is the root reduer
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = (state) => ({
+	currentUser: state.user.currentUser
 });
 
 export default connect(mapStateToProps)(Header);
