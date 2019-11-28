5407592d69c159783bcba0f77de61e06e32127c4
Diyar Faraj <diyar.faraj@gmail.com>
2019-11-28 17:22:13 +0100

    added cart selectors, replaced them with memoized selectors

---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,11 @@
+import { createSelector } from 'reselect';
+
+const selectCart = (state) => state.cart;
+
+const selectUser = (state) => state.user;
+
+export const selectCartItems = createSelector([ selectCart, selectUser ], (cart) => cart.cartItems);
+
+export const selectCartItemsCount = createSelector([ selectCartItems ], (cartItems) =>
+	cartItems.reduce((accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity, 0)
+);
