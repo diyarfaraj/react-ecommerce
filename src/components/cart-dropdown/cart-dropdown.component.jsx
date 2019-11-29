17f929c04fc187c762b4de269bb3e451a13b9442
Diyar Faraj <diyar.faraj@gmail.com>
2019-11-29 16:12:40 +0100

    added text if cart is empty

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
-import './cart-component.styles.scss';
+import './cart-dropdown.styles.scss';
 import CustomButton from './../custom-button/custom-button.component';
 import CartItem from './../cart-item/cart-item.component';
 import { selectCartItems } from './../../redux/cart/cart.selectors';
+import { createStructuredSelector } from 'reselect';
 
 const CartDropdown = ({ cartItems }) => (
 	<div className="cart-dropdown">
-		<div className="cart-items">{cartItems.map((cartItem) => <CartItem key={cartItem.id} item={cartItem} />)}</div>
+		<div className="cart-items">
+			{cartItems.length ? (
+				cartItems.map((cartItem) => <CartItem key={cartItem.id} item={cartItem} />)
+			) : (
+				<span className="empty-message">Your cart is empty</span>
+			)}
+		</div>
 		<CustomButton>GO TO CHECKOUT</CustomButton>
 	</div>
 );
 
-const mapStateToProps = (state) => ({
-	cartItems: selectCartItems(state)
+const mapStateToProps = createStructuredSelector({
+	cartItems: selectCartItems
 });
 
 export default connect(mapStateToProps)(CartDropdown);
