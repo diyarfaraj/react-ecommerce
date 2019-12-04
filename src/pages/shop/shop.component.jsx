a21f699e5b277df067bd293bc6046cc4f7c10d75
Diyar Faraj <48604250+diyarfaraj@users.noreply.github.com>
2019-12-04 22:44:56 +0100

    adding route and dynamic url params

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
+import { Route } from "react-router-dom";
+import CategoryPage from "../category/category.component";
 
-import CollectionsOverview from './../../components/collections-overview/collections-overview.componennt';
+import CollectionsOverview from "./../../components/collections-overview/collections-overview.componennt";
 
-const ShopPage = ({ collections }) => (
-	<div className="shop-page">
-		<CollectionsOverview />
-	</div>
+const ShopPage = ({ match }) => (
+  <div className="shop-page">
+    <Route exact path={`${match.path}`} componen={CollectionsOverview} />
+    <Route path={`${match.path}/:categoryId`} component={CategoryPage} />
+  </div>
 );
 
 export default ShopPage;
