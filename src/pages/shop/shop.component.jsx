19165277c8f08f8b76d55e4b35b3b15abf177fc1
Diyar Faraj <48604250+diyarfaraj@users.noreply.github.com>
2019-12-05 02:28:15 +0100

    added structure for collection pages

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import CategoryPage from "../category/category.component";
+import CollectionPage from "../collection/collection.component";
 
 import CollectionsOverview from "./../../components/collections-overview/collections-overview.componennt";
 
 const ShopPage = ({ match }) => (
   <div className="shop-page">
     <Route exact path={`${match.path}`} componen={CollectionsOverview} />
-    <Route path={`${match.path}/:categoryId`} component={CategoryPage} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
 );
 
