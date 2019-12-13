5a97567820cda46b7005de5ed7454089ccbcb802
Diyar Faraj <diyar.faraj@gmail.com>
2019-12-13 07:39:23 +0100

    added collection items to firestore db

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -14,12 +14,18 @@ const config = {
 	measurementId: 'G-2PCT8HJPGZ'
 };
 
+firebase.initializeApp(config);
+
 export const createUserProfileDocument = async (userAuth, additionalData) => {
 	if (!userAuth) return;
 
 	const userRef = firestore.doc(`users/${userAuth.uid}`);
+	/* const collectionRef = firestore.collection('users'); */
 
 	const snapShot = await userRef.get();
+	/* const collectionSnapshot = await collectionRef.get();
+
+	console.log({ collection: collectionSnapshot.docs.map((doc) => doc.data()) }); */
 
 	if (!snapShot.exists) {
 		const { displayName, email } = userAuth;
@@ -40,7 +46,19 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 	return userRef;
 };
 
-firebase.initializeApp(config);
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+	const collectionRef = firestore.collection(collectionKey);
+	console.log(collectionRef);
+
+	const batch = firestore.batch();
+	objectsToAdd.forEach((obj) => {
+		const newDocRef = collectionRef.doc();
+
+		batch.set(newDocRef, obj);
+	});
+
+	return await batch.commit();
+};
 
 //get the specific librarues of our need
 export const auth = firebase.auth();
