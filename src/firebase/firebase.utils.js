9caa0782b6686a962e41ff1fb3a8ab195ac4fc3a
Diyar Faraj <diyar.faraj@gmail.com>
2019-12-24 21:18:57 +0100

    config file chenged

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,9 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
-const dotenv = require('dotenv');
-
-dotenv.config();
 
 const config = {
 	apiKey: process.env.REACT_APP_APIKEY,
