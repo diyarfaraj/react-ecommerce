38c96a12938fba607d7f83f5809bf3d758ce2e39
Diyar Faraj <diyar.faraj@gmail.com>
2019-12-25 17:47:38 +0100

    successful payment done in dev through the server

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,29 +1,43 @@
-import React from "react";
-import StripeCheckout from "react-stripe-checkout";
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
-  const priceForStripe = price * 100;
-  const publishableKey = "pk_test_RattEfDluUm29dBhDp9NDDQJ00X0QWPcDl";
+	const priceForStripe = price * 100;
+	const publishableKey = 'pk_test_RattEfDluUm29dBhDp9NDDQJ00X0QWPcDl';
 
-  const onToken = token => {
-    console.log(token);
-    alert("Payment Succeded");
-  };
+	const onToken = (token) => {
+		axios({
+			url: 'payment',
+			method: 'post',
+			data: {
+				amount: priceForStripe,
+				token
+			}
+		})
+			.then((response) => {
+				alert('payment successful!');
+			})
+			.catch((error) => {
+				alert('payment FAILED!. Se console log');
+				console.log(JSON.parse(error));
+			});
+	};
 
-  return (
-    <StripeCheckout
-      label="Pay Now"
-      name="Kochi Haircare"
-      billingAddress
-      shippingAddress
-      image="https://svgshare.com/i/CUz.svg"
-      description={`Your total is $${price}`}
-      amount={priceForStripe}
-      panelLabel="Pay Now"
-      token={onToken}
-      stripeKey={publishableKey}
-    />
-  );
+	return (
+		<StripeCheckout
+			label="Pay Now"
+			name="Kochi Haircare"
+			billingAddress
+			shippingAddress
+			image="https://svgshare.com/i/CUz.svg"
+			description={`Your total is $${price}`}
+			amount={priceForStripe}
+			panelLabel="Pay Now"
+			token={onToken}
+			stripeKey={publishableKey}
+		/>
+	);
 };
 
 export default StripeCheckoutButton;
